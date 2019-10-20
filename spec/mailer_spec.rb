398a142deb6d95165ab3b14b5c204eb467b9271f
rafaelmatsumoto <rafaelmatsumoto43@gmail.com>
2019-10-19 23:44:15 -0300

    :white_check_mark: Updates mailing specs

---
+++ spec/mailer_spec.rb
@@ -0,0 +1,23 @@
+describe "sending an email" do
+  include Mail::Matchers
+
+  before(:each) do
+    Mail::TestMailer.deliveries.clear
+
+    Mail.deliver do
+      from 'mkt@slimcard.com'
+      to       'rafael@mailnator.com'
+      subject  'Slimcard'
+      body     'Testing'
+    end
+  end
+
+  it { is_expected.to have_sent_email } # passes if any email at all was sent
+
+  it { is_expected.to have_sent_email.from('mkt@slimcard.com') }
+  it { is_expected.to have_sent_email.to('rafael@mailnator.com') }
+
+  it { is_expected.to have_sent_email.matching_subject(/Slimcard?/) }
+  it { is_expected.to have_sent_email.matching_body(/Test(ing)?/) }
+
+end
